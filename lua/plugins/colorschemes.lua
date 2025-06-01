1722a3e1fc1ec6de1259cd6100966331488ca060
Muhammad Ikhsan <pararang@gmail.com>
2025-06-01 18:21:28 +0800

    wip

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -541,31 +541,15 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require("github-theme").setup({
-        options = {
-          compile_path = vim.fn.stdpath("cache") .. "/github-theme",
-          compile_file_suffix = "_compiled",
-          hide_end_of_buffer = true,
-          hide_nc_statusline = true,
-          transparent = false,
-          terminal_colors = true,
-          dim_inactive = false,
-          module_default = true,
-          styles = {
-            comments = "NONE",
-            functions = "NONE",
-            keywords = "NONE",
-            variables = "NONE",
-            conditionals = "NONE",
-            constants = "NONE",
-            numbers = "NONE",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-          },
-          inverse = { match_paren = false, visual = false, search = false },
-          darken = { floats = true, sidebars = { enable = true, list = {} } },
-        },
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = false,
+        hide_fillchars = false,
+        borderless_pickers = true,
+        terminal_colors = true,
+        cache = true,
+
+        -- vim.api.nvim_command("colorscheme cyberdream")
       })
     end,
   },
